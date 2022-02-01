3c30578b37349b09fbea07786c71fa50b0df374f
antsand <antsand2002@gmail.com>
2022-02-01 14:19:21 +0000

    Add comments and created a model file to store all logic

--- app/controllers/StatusController.php
+++ app/controllers/StatusController.php
@@ -1,15 +1,33 @@
 <?php
 declare(strict_types=1);
+/*
+  This controller file help display and modify the status of the boats. 
+  There are four main actions. 
+	Create -> Add a new boat 
+	Update -> Changes the status of the boat
+	Read -> Views the boat
+	Delete -> Removes a boat
+
+	The model is where the logic is stored. 
+	The model file is under the folder app/models/Status.php. 
+	Every action in this controller call the getdata() and putdata() function.
+
+	getdata() fetches the saved API file
+	putdata() saves the updated API file
+
+	The API file is stored under public/data/data.json 
+
+	Currently, there is no database attached to this application. Hence the data, for now, is stored in a file. 
+ */
 
 class StatusController extends ControllerBase
 {
-
+    /* This action adds a new boat */	
     public function createAction()
     {
         $response = new \Phalcon\Http\Response();
         $response->_isJsonResponse = true;
         $response->setContentType('application/json', 'UTF-8');
-	$response->setContent("this is it");
  	    $this->view->disable();
 	    $request = $this->request;
 	    $response = new \Phalcon\Http\Response();
@@ -17,72 +35,87 @@ class StatusController extends ControllerBase
 	    if ($request->isPost() && $request->isAjax()) {
 		$boat_selected = $request->getPost('boat_selected');
 		$guide_selected = $request->getPost('guide_selected');
-		$data_read = file_get_contents(__DIR__ . '/../../public/data/data.json');
-		$data_read = json_decode($data_read);
-		$boat_add = new StdClass();
-		$boat_add->status = 'docked';
-		$boat_add->name = $boat_selected;
-		$boat_add->guide = $guide_selected;
-		array_push($data_read->boats, $boat_add);
-		file_put_contents(__DIR__ . '/../../public/data/data.json', json_encode($data_read));
-		$response = new \Phalcon\Http\Response();
-		$response->_isJsonResponse = true;
-		$response->setContentType('application/json', 'UTF-8');
+		/* Using the Status model to fetch the API data. */
+		$status = new Status();
+		$data_read = $status->getdata();
+		if (!$data_read) {
+			$response->setContent(json_encode("Error reading file"));
+			return $response;
+		}	
+		/* Using the Status model to add a new boat. */
+		$status->adddata($data_read, $boat_selected, $guide_selected);
+		/* Using the Status model to save the API data. */
+		$status->putdata($data_read);
 		$response->setContent(json_encode($data_read));
 	    }
 	    return $response;
     }
-    
+
+    /* This action changes the status of the boat */  
     public function updateAction()
     {
  	    $this->view->disable();
 	    $request = $this->request;
 	    $response = new \Phalcon\Http\Response();
+	    $response->_isJsonResponse = true;
+	    $response->setContentType('application/json', 'UTF-8');
 	    $response->setContent(json_encode('Error'));
 	    if ($request->isPost() && $request->isAjax()) {
 		$boat_num = $request->getPost('boat_num');
 		$status_update = $request->getPost('status_update');
-		$data_read = file_get_contents(__DIR__ . '/../../public/data/data.json');
-		$data_read = json_decode($data_read);
+		/* using the Status model to fetch the API data. */
+		$status = new Status();
+		$data_read = $status->getdata();
+		if (!$data_read) {
+			$response->setContent(json_encode("Error reading file"));
+			return $response;
+		}	
 		$data_read->boats[intval($boat_num)]->status = $status_update;
-		file_put_contents(__DIR__ . '/../../public/data/data.json', json_encode($data_read));
-		$response = new \Phalcon\Http\Response();
-		$response->_isJsonResponse = true;
-		$response->setContentType('application/json', 'UTF-8');
+		$status->putdata($data_read);
 		$response->setContent(json_encode($data_read));
 	    }
 	    return $response;
     }
 
+    /* This action deletes one boat */
     public function deleteAction()
     {
  	    $this->view->disable();
 	    $request = $this->request;
 	    $response = new \Phalcon\Http\Response();
+	    $response->_isJsonResponse = true;
+	    $response->setContentType('application/json', 'UTF-8');
 	    $response->setContent(json_encode('Error'));
 	    if ($request->isPost() && $request->isAjax()) {
 		$boat_num = $request->getPost('boat_num');
-		$data_read = file_get_contents(__DIR__ . '/../../public/data/data.json');
-		$data_read = json_decode($data_read);
-		array_splice($data_read->boats, intval($boat_num), 1);
-		file_put_contents(__DIR__ . '/../../public/data/data.json', json_encode($data_read));
-		$response = new \Phalcon\Http\Response();
-		$response->_isJsonResponse = true;
-		$response->setContentType('application/json', 'UTF-8');
+		$status = new Status();
+		$data_read = $status->getdata();
+		if (!$data_read) {
+			$response->setContent(json_encode("Error reading file"));
+			return $response;
+		}
+		$status->removedata($data_read, $boat_num);	
+		$status->putdata($data_read);
 		$response->setContent(json_encode($data_read));
 	    }
 	    return $response;
 
     }
 
+    /* This action reads the data from the data.json API file */
     public function readAction()
     {
 	$this->view->disable();
-	$data_read = file_get_contents(__DIR__ . '/../../public/data/data.json');
         $response = new \Phalcon\Http\Response();
         $response->_isJsonResponse = true;
         $response->setContentType('application/json', 'UTF-8');
-	$response->setContent($data_read);
+	$status = new Status();
+	$data_read = $status->getdata();
+	if (!$data_read) {
+		$response->setContent(json_encode("Error reading file"));
+		return $response;
+	}
+	$response->setContent(json_encode($data_read));
 	return $response;
     }
 }
---
+++ app/models/Status.php
@@ -0,0 +1,38 @@
+<?php
+
+class Status extends Phalcon\Mvc\Model
+{
+	const STATUS_PATH = __DIR__ . '/../../public/data/data.json';
+
+	public function getdata() {
+		$api_file = file_get_contents($this::STATUS_PATH);
+		if ($api_file) {
+			return json_decode($api_file);
+		}
+		return null;
+	}
+
+	public function putdata($data, $json_true = FALSE) {
+		print_r($data);
+		if (!$json_true) {
+			$data = json_encode($data);
+		}
+		return file_put_contents(__DIR__ . '/../../public/data/data.json', $data);
+	}
+
+	public function adddata($data, $boat_selected, $guide_selected) {
+		$boat_add = new StdClass();
+		$boat_add->status = 'docked';
+		$boat_add->name = $boat_selected;
+		$boat_add->guide = $guide_selected;
+		array_push($data->boats, $boat_add);
+		return $data;
+	}
+	public function removedata($data, $index){
+		array_splice($data->boats, intval($index), 1);
+		return $data;
+
+	}
+}
+
+?>
